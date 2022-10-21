fddf2cf286d1b2742fb79c01d19eed0bc407a2b8
Siénou Lamien <sienou.lamien@student.vinci.be>
2022-10-21 10:48:21 +0200

    add exS6

---
+++ S6/ex/ex/Nortwind_API/Repository/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using ex.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ex.Nortwind_API.Repository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        NorthwindContext context = new NorthwindContext();
+
+        public BaseRepository(NorthwindContext dbContext)
+        {
+            context = dbContext;
+        }
+
+       async public Task DeleteAsync(TEntity entity)
+        {
+             context.Set<TEntity>().Remove(entity);
+        }
+
+         async public Task<IList<TEntity>> GetAllAsync()
+        {
+            IList<TEntity> lst = await context.Set<TEntity>().ToListAsync();
+            return lst;
+        }
+
+        async public  Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+
+       async  public Task InsertAsync(TEntity entity)
+        {
+           await context.Set<TEntity>().AddAsync(entity);
+
+        }
+
+       async public Task<bool?> SaveAsync(TEntity entity, Expression<Func<TEntity, bool>> predicate)
+        {
+            try
+            {
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException ex)
+            {
+
+                throw new DbUpdateException(ex.InnerException.Message);
+            }
+        }
+
+       async public Task<IList<TEntity>> SearchForAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+    }
+}
---
+++ S6/ex/ex/Nortwind_API/Repository/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+
+namespace ex.Nortwind_API.Repository
+{
+    public class EmployeeRepository : IRepository<EmployeeDTO>
+    {
+        public Task DeleteAsync(EmployeeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<EmployeeDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EmployeeDTO?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(EmployeeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool?> SaveAsync(EmployeeDTO entity, Expression<Func<EmployeeDTO, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<EmployeeDTO>> SearchForAsync(Expression<Func<EmployeeDTO, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
