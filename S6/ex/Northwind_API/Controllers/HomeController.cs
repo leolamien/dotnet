cc8d4bc87240128f0d4cf1760e870e11f27d530e
Siénou Lamien <sienou.lamien@student.vinci.be>
2022-10-28 10:26:56 +0200

    add_ex6

---
+++ S6/ex/Northwind_API/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind_API.Models;
+using System.Diagnostics;
+
+namespace Northwind_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+         {
+            HttpClient httpClient = _clientFactory.CreateClient(name: "Northwind_API");
+ 
+
+     HttpRequestMessage request = new HttpRequestMessage(method: HttpMethod.Get, requestUri: "/api/Employé");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            IList<EmployeModel> persons = await response.Content.ReadFromJsonAsync<IList<EmployeModel>>();
+
+            return View(persons);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ S6/ex/Northwind_API/Models/EmployeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Northwind_API.Models
+{
+    public class EmployeModel
+    {
+            public int EmployeeId { get; set; }
+            public string? LastName { get; set; }
+            public string? FirstName { get; set; }
+
+            public DateTime? BirthDate { get; set; }
+
+            public DateTime? HireDate { get; set; }
+
+
+            public string? Title { get; set; }
+
+            public string? TitleOfCourtesy { get; set; }
+        
+    }
+}
