059246368850436a4cd831830b92dae21f593cda
Siénou Lamien <sienou.lamien@student.vinci.be>
2022-11-25 10:33:36 +0100

    add ex S8

---
+++ S8/WpfEmployee/ViewModels/EmployeModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEmployee.Models;
+
+namespace WpfEmployee.ViewModels
+{
+    public class EmployeModel
+    {
+        private readonly Employee _employe;
+
+
+        public EmployeModel(Employee employe)
+        {
+            _employe = employe;
+        }
+
+        public int EmployeeId { get { return _employe.EmployeeId; } set { } }
+        public string LastName { get { return _employe.LastName; } set { } }
+        public string FirstName { get { return _employe.FirstName; } set { } }
+
+        public DateTime? DisplayBirthDate { get { return _employe.BirthDate; } set { } }
+        public DateTime? BirthDate { get { return _employe.BirthDate; } set { } }
+
+        public DateTime? HireDate { get { return _employe.HireDate; } set { } }
+
+
+        public string? Title { get { return _employe.Title; } set { } }
+
+        public string? TitleOfCourtesy { get { return _employe.TitleOfCourtesy; } set { } }
+        public string[] ListTitle { get { return _employe.TitleOfCourtesy; } set { } }
+
+        public string FullName { get { return String.Concat(_employe.FirstName+" " ,  _employe.LastName); } set { } }
+
+
+
+
+    }
+}
---
+++ S8/WpfEmployee/ViewModels/EmployeVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEmployee.Models;
+
+namespace WpfEmployee.ViewModels
+{
+    public class EmployeVM
+    {
+        private List<EmployeModel> _employeModels;
+        private NorthwindContext context = new NorthwindContext();
+        public List<EmployeModel> EmployeesList
+        {
+            get
+            {
+                //if (_legumesList == null) _legumesList = loadLegumes2();
+                //return _legumesList;
+                return ConcatName();
+            }
+        }
+
+        public List<string> ListTitle()
+        {
+            return liste();
+        };
+        private List<EmployeModel> ConcatName()
+        {
+            List<EmployeModel> localCollection = new List<EmployeModel>();
+            foreach (var item in context.Employees)
+            {
+                localCollection.Add(new EmployeModel(item));
+
+            }
+
+            return localCollection;
+        }
+
+        private List<string> liste()
+        {
+            List<string> localCollection = new List<string>();
+            foreach (var item in context.Employees)
+            {
+                foreach (var ti in item.Title)
+                {
+                    localCollection.Add(ti);
+                }
+
+            }
+
+            return localCollection;
+        }
+    }
+}
